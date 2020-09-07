b8b3520b2d6698c744d87aaa19eba90184a5c979
Lybfip Ruxeacrh <ct9a5820@gmail.com>
2020-09-07 03:14:59 +0000

    Add wall_stop.launch file

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,12 @@
 #!/bin/bash -xve
 
-#sync add make
+# Copy repository to workspace
 rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+# Bring pimouse_ros to your workspace with git clone
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/Lybfip/pimouse_ros.git
+        # If depth=1 is specified, only the latest one can be cloned.
+
 cd ~/catkin_ws
 catkin_make
